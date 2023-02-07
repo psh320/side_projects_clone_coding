f5aaeede937a0cd0d3be8b2bd82888ef1ee2499d
Sungho Park <43461389+psh320@users.noreply.github.com>
2023-02-07 15:53:34 +0800

    Delete animalcrossing directory

--- animalcrossing/src/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux';
-import authReducer from './authReducer';
-import fishReducer from './fishReducer';
-
-export default combineReducers({
-	auth: authReducer,
-	fish: fishReducer
-});
